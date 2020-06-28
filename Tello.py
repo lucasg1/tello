ec5138eebda210fa6cd6e0ad1c04c5669111a4b1
lucasg1 <lucas97bg@gmail.com>
2020-06-28 15:06:33 -0300

    foi

--- Tello.py
+++ Tello.py
@@ -50,55 +50,66 @@ print ("Current battery is " + tello.get_battery())
 tello.takeoff()
 time.sleep(9)
 
+#takePicture()
+#frame_read = tello.get_frame_read()
+#frame = frame_read.frame
+#cv2.imshow("Video", frame)
+
+#tello.land()
+#tello.streamoff()
+#cv2.destroyAllWindows()
+#sys.exit()
+
+
 tello.move_up(82)
 time.sleep(2)
 
-#frame_read = tello.get_frame_read()
-#stop = False
+frame_read = tello.get_frame_read()
+stop = False
 
-#face_cascade = cv2.CascadeClassifier("face detector.xml")
+face_cascade = cv2.CascadeClassifier("face detector.xml")
 
-#while not stop:
+while not stop:
 
-#    frame = frame_read.frame
-#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    frame = frame_read.frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-#    if state == State.detectingFaces:
-#        tello.rotate_clockwise(20)
-#        time.sleep(0.1)
+    if state == State.detectingFaces:
+        tello.rotate_clockwise(20)
+        time.sleep(0.1)
 
-#    x_pos = None
-#    y_pos = None
-#    width = None
-#    height = None
+    x_pos = None
+    y_pos = None
+    width = None
+    height = None
 
-#    for (x,y,w,h) in faces:
-#        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-#        x_pos = x
-#        y_pos = y
-#        width = w
-#        height = h
-#        if state == State.detectingFaces:
-#            state = State.goingToPerson
-#            print ("X and Y are: " + str(x) + ", " + str(y) + " and width and height are: " + str(w) + ", " + str(h))
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        x_pos = x
+        y_pos = y
+        width = w
+        height = h
+        if state == State.detectingFaces:
+            state = State.goingToPerson
+            print ("X and Y are: " + str(x) + ", " + str(y) + " and width and height are: " + str(w) + ", " + str(h))
 
-#    if width != None and height != None:
-#        if width >= distanceThreshold:
-#            takePicture()
-#            tello.land()
-#            break
-#        else:
-#            tello.move_forward(35)
-#            time.sleep(1)
+    if width != None and height != None:
+        if width >= distanceThreshold:
+            # takePicture()
+            cv2.imwrite('foi.png',frame)
+            tello.land()
+            break
+        else:
+            tello.move_forward(35)
+            time.sleep(1)
 
+    cv2.imshow("Video", frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
 
-#    cv2.imshow("Video", frame)
-#    key = cv2.waitKey(1)
-#    if key == ord('q'):
-#        break
-
-#    time.sleep(1/FPS)
+    time.sleep(1/FPS)
 
 tello.land()
 tello.streamoff()
